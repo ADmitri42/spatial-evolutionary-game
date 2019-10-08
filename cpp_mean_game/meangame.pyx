62c026314bd9517524746571aae93d0482095a06
ADmitri <admitri42@gmail.com>
2019-10-08 21:55:36 +0300

    Fix problem with symbol

--- cpp_mean_game/meangame.pyx
+++ cpp_mean_game/meangame.pyx
@@ -1,12 +1,11 @@
 # distutils: language = c++
+# distutils: sources = evolve.cc
 import numpy as np
 
 from libcpp.vector cimport vector
-from numpy cimport import_array, PyArray_SimpleNewFromData, NPY_INT, npy_intp
+from numpy cimport import_array, PyArray_SimpleNewFromData, NPY_INT, npy_intp, NPY_DOUBLE
 
-# from evolve cimport Game
-# OPT="-DNDEBUG -g -fwrapv -O3 -Wall -Wstrict-prototypes" python3 setup.py build_ext
-# export CFLAGS='-I/home/admitri/.local/lib/python3.6/site-packages/numpy/core/include'
+
 cdef extern from "evolve.h":
     cdef cppclass Game:
         Game(int, double)
@@ -30,7 +29,7 @@ cdef class GameField:
 
     def __cinit__(self, int L, double b):
         self.c_game = new Game(L, b)
-        _L = L
+        self._L = L
 
     def __dealloc__(self):
         del self.c_game
@@ -61,7 +60,7 @@ cdef class GameField:
         cdef vector[int] vec;
         vec.resize(self._L*self._L)
         for j in range(arr.size):
-            self.vec[j] = arr[j]
+            vec[j] = arr[j]
 
         self.c_game.set_field(vec)
 
@@ -72,6 +71,12 @@ cdef class GameField:
     @b.setter
     def b(self, double arr):
         self.c_game.set_b(arr)
+
+    @property
+    def densities(self):
+        cdef npy_intp dims[1]
+        dims[0] = self.c_game.get_densities_size()
+        return PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, self.c_game.get_densities_pointer())
 
     def evolve(self, int num_steps = 1):
         self.c_game.evolve(num_steps)
--- cpp_mean_game/setup.py
+++ cpp_mean_game/setup.py
@@ -1,11 +1,20 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 import numpy as np
 
+# OPT="-DNDEBUG -g -fwrapv -O3 -Wall -Wstrict-prototypes" python3 setup.py build_ext
+# export CFLAGS='-I/home/admitri/.local/lib/python3.6/site-packages/numpy/core/include'
 
-# OPT = -DNDEBUG -g -fwrapv -O3 -Wall -Wstrict-prototypes
-setup(ext_modules = cythonize(
-           ["meangame.pyx", "evolve.cc"],                 # our Cython source
-           include_path = [np.get_include(),],
-           language="c++",             # generate C++ code
-      ))
+# setup(ext_modules = cythonize(
+#            ["meangame.pyx", "evolve.cc"],                 # our Cython source
+#            include_path = [np.get_include(),],
+#            language="c++",             # generate C++ code
+#       ))
+
+setup(ext_modules=[Extension("meangame",
+                             ["meangame.pyx", "evolve.cc"],
+                             language="c++",
+                             include_path = [np.get_include(),])],
+      cmdclass = {'build_ext': build_ext})
