7e6142eaa81cacdc9c6efbd3f1a29790be653cc6
ADmitri <admitri42@gmail.com>
2019-12-12 23:45:39 +0300

    Updated README and added persistance calculating

--- cpp_mean_game/argparser.cpp
+++ cpp_mean_game/argparser.cpp
@@ -6,11 +6,18 @@
 #include <iostream>
 #include <sstream>
 
+
+/*
+ * Function to parse each argument
+ *
+ * TO-DO:
+ *  - Directory with fields
+ */
 Config set_field(std::string arg, Config config, bool mess = true){
     std::stringstream ss;
     config->successful = true;
 
-    if(arg.rfind("-L", 0) == 0){
+    if(arg.rfind("-L", 0) == 0){ // Field size
         int L;
         ss << arg.substr(2, arg.size());
         if(ss >> L){
@@ -19,7 +26,7 @@ Config set_field(std::string arg, Config config, bool mess = true){
             config->successful = false;
             std::cerr << "Expected integer, got " << arg << std::endl;
         }
-    }else if(arg.rfind("-NSteps", 0) == 0){
+    }else if(arg.rfind("-NSteps", 0) == 0){ // Total number of steps
         int NSteps;
         ss << arg.substr(7, arg.size());
         if(ss >> NSteps){
@@ -28,7 +35,7 @@ Config set_field(std::string arg, Config config, bool mess = true){
             config->successful = false;
             std::cerr << "Expected integer, got " << arg << std::endl;
         }
-    }else if(arg.rfind("-NDrop", 0) == 0){
+    }else if(arg.rfind("-NDrop", 0) == 0){ // Number of steps to be discarded
         int NDrop;
         ss << arg.substr(6, arg.size());
         if(ss >> NDrop){
@@ -37,7 +44,7 @@ Config set_field(std::string arg, Config config, bool mess = true){
             config->successful = false;
             std::cerr << "Expected integer, got " << arg << std::endl;
         }
-    }else if(arg.rfind("-NFields", 0) == 0){
+    }else if(arg.rfind("-NFields", 0) == 0){ // Number of fields
         int NFields;
         ss << arg.substr(8, arg.size());
         if(ss >> NFields){
@@ -46,11 +53,11 @@ Config set_field(std::string arg, Config config, bool mess = true){
             config->successful = false;
             std::cerr << "Expected integer, got " << arg << std::endl;
         }
-    }else if(arg.rfind("-h", 0) == 0){
+    }else if(arg.rfind("-h", 0) == 0){ // Help
         config->help = true;
         config->successful = false;
 
-    }else if (mess){
+    }else if (mess){ // If we need to print message
         config->successful=false;
         std::cerr << "Unknown parameter " << arg << std::endl;
     } else {
@@ -59,11 +66,14 @@ Config set_field(std::string arg, Config config, bool mess = true){
     return config;
 }
 
+/*
+ * Show help message
+ */
 void help_message(std::string prog_name){
     std::cout <<  "Usage: " << prog_name << " [OPTIONS] output_file" << std::endl;
     std::cout << "Options: " << std::endl;
-    std::cout << "\t-L{integer} - Size of the field\n\t-NSteps - Number of steps" << std::endl;
-    std::cout << "\t-NDrop - number of steps, which would be dropped from output file" << std::endl;
+    std::cout << "\t-L{integer} - Size of the field\n\t-NSteps - Total number of steps" << std::endl;
+    std::cout << "\t-NDrop - Number of steps to be discarded from output" << std::endl;
     std::cout << "\t-NFields - number of fields in directory 'fields/' in form 'field_{L}_{n}.npy'" << std::endl;
 }
 
@@ -72,11 +82,15 @@ std::string err_message(std::string prog_name){
     std::cerr << "-h will help you" << std::endl;
 }
 
+
+/*
+ * Parsing command-line arguments into config
+ */
 Config get_config(int argc, char* argv[]){
     Config config = new pConfig;
     config->total_steps = 16000;
     config->drop_steps = 8000;
-    config->L = 400;
+    config->L = 100;
     config->fields_number = 40;
     config->successful = true;
     config->fields_directory = "fields";
@@ -102,7 +116,7 @@ Config get_config(int argc, char* argv[]){
     }else{
         for(int i = 1; i < argc; i++){
 
-            set_field(argv[i], config, !(i == argc-1)   );
+            set_field(argv[i], config, i != argc-1);
 
             if(config->help){
                 help_message(argv[0]);
