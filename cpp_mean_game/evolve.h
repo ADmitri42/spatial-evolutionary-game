be622545a098863a5382fc74d1bfbe1c0a905a52
ADmitri <admitri42@gmail.com>
2019-12-17 21:23:09 +0300

    Added persistence

--- cpp_mean_game/evolve.h
+++ cpp_mean_game/evolve.h
@@ -5,27 +5,32 @@
 
 class MeanGame{
 private:
-    std::vector<int> field;
+    std::vector<char> field;
+    std::vector<char> unchanged;
+
     std::vector<double> densities;
+
     int L;
     double b;
+    int perCalFrom;
+    int perCalTill;
 
 public:
     MeanGame(int size, double _b=1.8);
 
-    void evolve(int num_steps = 0);
+    void evolve(int num_steps = 0, int percfrom = -1, int perctill = -1);
 
     std::vector<double> get_densities();
     int size();
     double get_b();
     void set_b(double new_b);
-    std::vector<int> get_field();
+    std::vector<char> get_field();
     void set_field(const std::vector<int> &new_field);
-
+    double get_persistence();
     /*
      * Function to create numpy array
      */
-    int* get_field_pointer();
+    char* get_field_pointer();
     int get_densities_size();
     double* get_densities_pointer();
 };
