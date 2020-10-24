75fec21b0df656063818f70ad85384cd8be43c01
ADmitri42 <ADmitri42@gmail.com>
2020-10-24 10:47:53 +0300

    refactor: Moved realization of two field game and renamed it

--- games/cpp/games.h
+++ games/cpp/games.h
@@ -31,4 +31,33 @@ public:
     void update_field(const std::vector<double> &scores, int time_moment, int percfrom = -1, int perctill = -1);
 };
 
+/*
+ * Double field basic game
+ * 
+ * Two field stored one after th other
+ * Density for field 1 has indeces 2n
+ * For field 2 indeces 2n+1
+ */
+
+class DoubleMeanFieldGame: public AbstractSpatialGame{
+protected:
+    double b1, b2;
+
+public:
+    DoubleMeanFieldGame(size_t size, double _b1=1.6, double _b2=1.6);
+
+    virtual void calculate_scores(std::vector<double> &scores);
+    virtual void update_field(const std::vector<double> &scores, int time_moment, int percfrom = -1, int perctill = -1);
+    void evolve(int num_steps = 0, int percfrom = -1, int perctill = -1);
+
+    std::array<double, 2> get_bs();
+    void set_b(double new_b1, double new_b2);
+    void set_field(const std::vector<int> &new_field1, const std::vector<int> &new_field2);
+    std::array<double, 2> get_persistence();
+    /*
+     * Function to create numpy array
+     */
+    int get_densities_size();
+};
+
 #endif
--- games/cpp/spatgame.h
+++ games/cpp/spatgame.h
@@ -43,36 +43,4 @@ public:
     double* get_densities_pointer();
 };
 
-/*
- * Double field basic game
- * 
- * Two field stored one after th other
- * Density for field 1 has indeces 2n
- * For field 2 indeces 2n+1
- */
-
-class AbstractSpatialGame2Field: public AbstractSpatialGame{
-protected:
-    double b1, b2;
-
-public:
-    AbstractSpatialGame2Field(size_t size, double _b1=1.6, double _b2=1.6);
-    virtual ~AbstractSpatialGame2Field() {};
-
-    virtual void calculate_scores(std::vector<double> &scores);
-    virtual void update_field(const std::vector<double> &scores, int time_moment, int percfrom = -1, int perctill = -1);
-    void evolve(int num_steps = 0, int percfrom = -1, int perctill = -1);
-
-    std::vector<double> get_b();
-    void set_b(double new_b1, double new_b2);
-    void set_field(const std::vector<int> &new_field1, const std::vector<int> &new_field2);
-    std::vector<double> get_persistence();
-    /*
-     * Function to create numpy array
-     */
-    char* get_field_pointer();
-    int get_densities_size();
-    double* get_densities_pointer();
-};
-
 #endif //SPATIAL_EVOLUTIONARY_GAME_SPATGAME_H
